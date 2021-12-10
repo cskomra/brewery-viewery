510745ac4ea029d29b12d89130008655e92bc908
Connie Skomra <connie@cskomra.com>
2021-12-10 09:17:59 -0500

    BeerList use Beer component

--- src/components/beer-list/beer-list.component.js
+++ src/components/beer-list/beer-list.component.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './beer-list.styles.css'
+import {Beer} from '../beer/beer.component';
+import './beer-list.styles.css';
 
 export const BeerList = (props) => (
   <div className='beer-list'>
       {props.beers.map( beer => (
-        <h1 key={beer.id}>{beer.name}</h1>
+        <Beer key={beer.id} beer={beer}/>
       ))} 
     </div>
 )
---
+++ src/components/beer/beer.component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Beer = (props) => (
+  <div>
+    <h1> {props.beer.name} </h1>
+  </div>
+)
