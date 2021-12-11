0894dfac9ce2cce3ee51c83a1c1ce73aadfcf70e
Connie Skomra <connie@cskomra.com>
2021-12-11 10:21:47 -0500

    sort beers by ABV

--- src/components/beer-list/beer-list.component.js
+++ src/components/beer-list/beer-list.component.js
@@ -2,10 +2,17 @@ import React from 'react';
 import {Beer} from '../beer/beer.component';
 import './beer-list.styles.css';
 
-export const BeerList = (props) => (
-  <div className='beer-list'>
-      {props.beers.map( beer => (
-        <Beer key={beer.id} beer={beer}/>
-      ))} 
-  </div>
-)
+export const BeerList = (props) => {
+
+  props.beers.sort(function (a, b){
+    return b.abv - a.abv;
+  })
+
+  return (
+    <div className='beer-list'>
+        {props.beers.map( beer => (
+          <Beer key={beer.id} beer={beer}/>
+        ))} 
+    </div>
+  )
+}
--- src/components/beer/beer.component.js
+++ src/components/beer/beer.component.js
@@ -6,21 +6,19 @@ import { Alert } from '@mui/material';
 
 export const Beer = (props) => {
 
-  const beerString = JSON.stringify(props.beer);
-  //console.log(beerString);
-  
+  const beerString = JSON.stringify(props.beer);  
   const dryHopSuccessAlert = beerString.includes('dry hop') ? <Alert severity='success'><strong>Dry Hopped</strong>.</Alert> : null;
   const lactoseWarningAlert = beerString.includes('lactose') ? <Alert severity='warning'><strong>Contains Lactose</strong>.</Alert> : null;
   
   return(
     <Box component="span" className='beer-container'>
-      <p>id: {props.beer.id}</p>
       <h1> {props.beer.name} </h1>
-      <p> {props.beer.tagline} </p>
-      <p> {props.beer.description} </p>
 
       {lactoseWarningAlert}
       {dryHopSuccessAlert}
+
+      <p> {props.beer.tagline} </p>
+      <p> {props.beer.description} </p>
 
       <img src={props.beer.image_url} alt='{props.beer.name}'></img>
       <p> ABV: {props.beer.abv} </p>
